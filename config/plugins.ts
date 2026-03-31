411d5e824f549d7071ff784fdec3a77bd879b79f
suhang <liyuxing@bilibili.com>
2026-03-31 21:05:27 +0800

    add s3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,15 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
-export default config;
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+        region: env('AWS_REGION'),
+        endpoint: env('AWS_ENDPOINT'),
+        params: { Bucket: env('AWS_BUCKET') },
+        baseUrl: env('CLOUDFLARE_PUBLIC_URL'),
+      },
+    },
+  },
+});
