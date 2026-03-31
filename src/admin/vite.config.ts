22b8947a612b0521ac4ba640f38d6c7eb49dc987
suhang <liyuxing@bilibili.com>
2026-03-31 21:20:06 +0800

    add allowedHosts

---
+++ src/admin/vite.config.ts
@@ -0,0 +1,7 @@
+import { mergeConfig, type UserConfig } from 'vite';
+
+export default (config: UserConfig) => {
+  return mergeConfig(config, {
+    server: { allowedHosts: ['worldtravelgo.cn'] },
+  });
+};
